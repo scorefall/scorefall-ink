d1ad549d13b8e9f756b75775c96e9384979131f3
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-08-13 12:06:16 -0500

    Add Program struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // ScoreFall Studio - Music Composition Software
 //
-// Copyright (C) 2019 Jeron Aldaron Lau <jeronlau@plopgrizzly.com>, Doug P. Lau
+// Copyright (C) 2019 Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
+// Copyright (C) 2019 Doug P. Lau
 //
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
@@ -14,3 +15,34 @@
 //
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use scof::Scof;
+
+/// This is the entire program context.
+pub struct Program {
+    /// The save file.
+    pub scof: Scof,
+    // Which channel is the cursor at.
+    chan: usize,
+    // Which bar is the cursor at.
+    bar: usize,
+    // Cursor within the measure (not necessarily at a specific time).
+    curs: usize,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Default for Program {
+    fn default() -> Self {
+        Self {
+            scof: Scof::default(),
+            bar: 0,
+            curs: 0,
+            chan: 0,
+        }
+    }
+}
