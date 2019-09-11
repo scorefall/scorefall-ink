d32b0b0724a42b0b8d912fcdadecc54575759f35
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-09-10 20:50:29 -0500

    Add set_dur function.

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use scof::{Cursor, Marking, Scof};
+use scof::{Cursor, Marking, Scof, Fraction, IsZero, Note};
 
 /// This is the entire program context.
 pub struct Program {
@@ -67,13 +67,13 @@ impl Program {
                 Marking::Dynamic(_) => {/*Do nothing*/},
                 Marking::GraceInto(note) => {
                     self.scof.set_pitch(&self.cursor, if up { note.step_up(create) } else { note.step_down(create) }.pitch.unwrap())
-                },
+                }
                 Marking::GraceOutOf(note) => {
                     self.scof.set_pitch(&self.cursor, if up { note.step_up(create) } else { note.step_down(create) }.pitch.unwrap())
-                },
+                }
                 Marking::Note(note) => {
                     self.scof.set_pitch(&self.cursor, if up { note.step_up(create) } else { note.step_down(create) }.pitch.unwrap())
-                },
+                }
                 Marking::Breath => {/*Do nothing*/},
                 Marking::CaesuraShort => {/*Do nothing*/},
                 Marking::CaesuraLong => {/*Do nothing*/},
@@ -102,11 +102,86 @@ impl Program {
 
     /// Move a note down 1 step within the key.
     pub fn down_half_step(&mut self) {
-        self.down_half_step();
+        // FIXME
+        self.down_step();
     }
 
     /// Move a note up 1 step within the key.
     pub fn up_half_step(&mut self) {
-        self.up_half_step();
+        // FIXME
+        self.up_step();
+    }
+
+    /// Set duration of a note.
+    pub fn set_dur(&mut self, num: u8, den: u8) {
+        if let Some(mark) = self.scof.marking(&self.cursor) {
+            let dur = Fraction::new(num, den);
+
+            match mark {
+                Marking::Dynamic(_) => {/*Do nothing*/},
+                Marking::GraceInto(note) => {
+                    self.scof.set_duration(&self.cursor, dur)
+                }
+                Marking::GraceOutOf(note) => {
+                    self.scof.set_duration(&self.cursor, dur)
+                },
+                Marking::Note(note) => {
+                    let old_duration = note.duration;
+                    if old_duration > dur {
+                        // Insert Rests
+                        let rem = old_duration - dur; // TODO: Test Code Sub
+//                        while !rem.is_zero() {
+                            self.scof.insert_after(&self.cursor, Note {
+                                pitch: None,
+                                duration: rem,
+                                articulation: None,
+                            }).unwrap();
+//                        }
+                    } else {
+                        // Delete Notes
+                        let mut rem = dur - old_duration;
+                        while !rem.is_zero() {
+                            if let Some(marking) = self.scof.remove_after(&self.cursor) {
+                                if marking.duration <= rem {
+                                    rem = rem - marking.duration;
+                                } else {
+                                    self.scof.insert_after(&self.cursor, Note {
+                                        pitch: None,
+                                        duration: marking.duration - rem,
+                                        articulation: None,
+                                    });
+                                    break;
+                                }
+                            } else {
+                                // FIXME: Algorithm Over barlines.
+                            }
+                        }
+                    }
+                    self.scof.set_duration(&self.cursor, dur)
+                },
+                Marking::Breath => {/*Do nothing*/},
+                Marking::CaesuraShort => {/*Do nothing*/},
+                Marking::CaesuraLong => {/*Do nothing*/},
+                Marking::Cresc => {/*Do nothing*/},
+                Marking::Dim => {/*Do nothing*/},
+                Marking::Pizz => {/*Do nothing*/},
+                Marking::Arco => {/*Do nothing*/},
+                Marking::Mute => {/*Do nothing*/},
+                Marking::Open => {/*Do nothing*/},
+                Marking::Repeat => {/*Do nothing*/},
+            }
+        } else {
+            // Shouldn't happen, do nothing.
+        }
+    }
+
+    /// Set duration of a note to tuplet.
+    pub fn tuplet(&mut self) {
+        // FIXME
+    }
+
+    /// Set duration of note to dotted.
+    pub fn dotted(&mut self) {
+        // FIXME
     }
 }
