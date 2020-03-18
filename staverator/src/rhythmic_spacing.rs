f2e0660fff55928a0a0d09d781a570cd4021dad2
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-03-18 11:35:33 -0500

    Correct width of first note cursor.

--- staverator/src/rhythmic_spacing.rs
+++ staverator/src/rhythmic_spacing.rs
@@ -109,12 +109,15 @@ impl<'a, 'b, 'c> BarEngraver<'a, 'b, 'c> {
                 if stave_i == stave_j {
                     self.cursor = None;
                     let e = if x == 0.0 { 0 } else { -Stave::SPACE / 2 };
+                    let f = if x == 0.0 { -Stave::SPACE / 2 } else { 0 };
                     let x =
                         Stave::MARGIN_X + (super::BAR_WIDTH as f32 * x) as i32;
                     cursor_rect = Some((
-                        x + e,                                             // X
-                        0i32,                                              // Y
-                        (super::BAR_WIDTH as f32 * self.width) as i32 - x, // W
+                        x + e, // X
+                        0i32,  // Y
+                        (super::BAR_WIDTH as f32 * self.width) as i32 - x
+                            + f
+                            + Stave::MARGIN_X, // W
                         self.bar.height(),
                     ));
                 }
@@ -216,18 +219,18 @@ fn clamp(a: f32, min: f32, max: f32) -> f32 {
 fn get_spacing(duration: u16) -> f32 {
     let dur = duration as f32;
     match duration {
-        1..=7 => lerp(2.25, 2.375, clamp(dur, 1.0, 8.0)), // 128th-Sixteenth
+        1..=7 => lerp(2.25, 2.375, clamp(dur, 1.0, 8.0)), // 128th-16th
         8..=15 => lerp(2.375, 2.5, clamp(dur, 8.0, 16.0)), // Sixteenth
         16..=23 => lerp(2.5, 3.0, clamp(dur, 16.0, 24.0)), // Eighth
-        24..=31 => lerp(3.0, 3.5, clamp(dur, 24.0, 32.0)), // Dotted Eighth
+        24..=31 => lerp(3.0, 3.5, clamp(dur, 24.0, 32.0)), // Dot'd Eighth
         32..=47 => lerp(3.5, 4.0, clamp(dur, 32.0, 48.0)), // Quarter
-        48..=63 => lerp(4.0, 5.0, clamp(dur, 48.0, 64.0)), // Dotted Quarter
+        48..=63 => lerp(4.0, 5.0, clamp(dur, 48.0, 64.0)), // Dot'd Quarter
         64..=95 => lerp(5.0, 6.0, clamp(dur, 64.0, 96.0)), // Half
         96..=127 => lerp(6.0, 7.0, clamp(dur, 96.0, 128.0)), // Dotted Half
         128..=255 => lerp(7.0, 8.0, clamp(dur, 128.0, 256.0)), // Whole
-        256..=383 => lerp(8.0, 9.0, clamp(dur, 256.0, 384.0)), // Dotted Whole
-        384..=511 => lerp(9.0, 10.0, clamp(dur, 384.0, 512.0)), // Double Whole
-        512 => 10.0,                                      // Quadruple Whole
+        256..=383 => lerp(8.0, 9.0, clamp(dur, 256.0, 384.0)), // Dot'd Whole
+        384..=511 => lerp(9.0, 10.0, clamp(dur, 384.0, 512.0)), // Breve
+        512 => 10.0,                                      // Longa
         _ => panic!("Bug in Notator, no glyph for ({})", duration),
     }
 }
