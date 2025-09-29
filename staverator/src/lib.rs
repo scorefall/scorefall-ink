bb03db83156b1b02742ec379d65a7e814a523eae
Douglas Lau <doug.p.lau@gmail.com>
2025-09-29 07:09:39 -0500

    Update staverator/src/lib.rs
    Co-authored-by: Jeryn Aldaron Lau <aldaronlau@gmail.com>

--- staverator/src/lib.rs
+++ staverator/src/lib.rs
@@ -422,10 +422,10 @@ impl BarElem {
             let mut html = Html::new();
             Svg::new(&mut html)
                 .rect()
-                .x(format!("{x}"))
-                .y(format!("{y}"))
-                .width(format!("{width}"))
-                .height(format!("{height}"))
+                .x(x.to_string())
+                .y(y.to_string())
+                .width(width.to_string())
+                .height(height.to_string())
                 .end();
             self.elements.push(html);
             count += 2;
