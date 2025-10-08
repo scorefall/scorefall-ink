876aaaa130e0d5711a6a3b62b8fa82d0bccdbf05
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2025-10-07 22:53:35 -0500

    Try fix warning

--- ci/rustfmt.sh
+++ ci/rustfmt.sh
@@ -1,2 +1,4 @@
-rustup toolchain install $1 --profile=minimal -c rustfmt
-cargo +$1 fmt --all --check
+rustup toolchain install $1 --profile=minimal -c rustfmt && \
+    echo "Checking format..." && \
+    cargo +$1 fmt --all --check && \
+    echo "Format check passed"
