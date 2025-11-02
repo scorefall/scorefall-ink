76a65e68423c4d6127aa5fdd87e3752f2743da2a
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2025-11-02 00:05:27 -0500

    Split out build.sh for deployment to website

--- web/release.sh
+++ web/release.sh
@@ -1,6 +1,1 @@
-RUSTFLAGS="--remap-path-prefix=$PWD=_ --remap-path-prefix=$HOME/.local/lib/cargo=- --remap-path-prefix=$HOME/.local/lib/rustup=+ --remap-path-prefix=$HOME=%" cargo build --target wasm32-unknown-unknown --release && \
-    wasm-bindgen --out-dir site/gen --target web --no-typescript --remove-name-section --remove-producers-section --omit-default-module-path ../target/wasm32-unknown-unknown/release/ink.wasm && \
-    wasm-opt site/gen/ink_bg.wasm -o site/gen/ink_bg.wasm -Os && \
-    wasm-strip site/gen/ink_bg.wasm && \
-    ls -l site/gen/ink_bg.wasm && \
-    http site
+./build.sh && http site
