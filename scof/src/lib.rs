56f41b62ec653206a4c4d90fee851e598dc4f727
Doug Lau <doug.p.lau@gmail.com>
2025-11-09 08:57:10 -0600

    Split out scof synth module

--- scof/src/lib.rs
+++ scof/src/lib.rs
@@ -28,6 +28,7 @@ mod marking;
 mod measure;
 mod movement;
 pub mod note;
+mod synth;
 
 pub use crate::{
     chan::{Chan, Channel},
@@ -40,90 +41,8 @@ pub use crate::{
         Articulation, Note, Pitch, PitchAccidental, PitchClass, PitchName,
         PitchOctave, Steps,
     },
+    synth::{Instrument, Synth},
 };
-
-/////////////////////
-
-/// A waveform.
-#[allow(unused)] // FIXME: Have ability to use waveform
-pub struct Waveform {
-    /// True: Signed 16-bit integer, False: Signed 8-bit integer.
-    si16: bool,
-    /// True: Waveform doesn't loop, False: Waveform loops.
-    once: bool,
-    /// Hexadecimal string representation of waveform.
-    wave: String,
-}
-
-/// Reverb & other effect settings.
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct Effect {
-    // TODO
-}
-
-/// Channel definition for synthesis.
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct SynthChan {
-    /// Name of instrument sounds.
-    waveform: Vec<String>,
-    /// Instrument effects.
-    effect: Vec<u32>,
-    //    ///
-    //    sounds: Vec<Sound>,
-    /// Volume: 0-1
-    volume: f32,
-}
-
-/// Synthesis file.
-#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-pub struct Synth {
-    /// Instrument presets,
-    /// Reverb presets, IDs automatically assigned.
-    effect: Vec<Effect>,
-    /// Channels
-    chan: Vec<SynthChan>,
-}
-
-/// An instrument in the soundfont for this score.
-#[derive(PartialEq, Debug, Default, Serialize, Deserialize)]
-pub struct Instrument {
-    // Default waveform for instrument.
-    waveform: String,
-    // Straight or Palm mute depending on instrument.
-    mute: Option<String>,
-    // Cup mute.
-    cup_mute: Option<String>,
-    // Wah-wah mute.
-    harmon_mute: Option<String>,
-    // Plunger mute.
-    plunger_mute: Option<String>,
-    // Harmonic (for guitar)
-    harmonic: Option<String>,
-
-    // Use different waveform for this dynamic
-    ppp: Option<String>,
-    // Use different waveform for this dynamic
-    pp: Option<String>,
-    // Use different waveform for this dynamic
-    p: Option<String>,
-    // Use different waveform for this dynamic
-    mp: Option<String>,
-    // Use different waveform for this dynamic
-    mf: Option<String>,
-    // Use different waveform for this dynamic
-    f: Option<String>,
-    // Use different waveform for this dynamic
-    ff: Option<String>,
-    // Use different waveform for this dynamic
-    fff: Option<String>,
-}
-
-/*/// A soundfont used in the score (either in the .scof or a .sfsf and linked to).
-#[derive(PartialEq, Debug, Default, Serialize, Deserialize)]
-pub struct SoundFont {
-    /// A list of instruments.
-    pub instrument: Vec<Instrument>,
-}*/
 
 fn default_symtime() -> bool {
     false // Don't show a symbol for time signature.
